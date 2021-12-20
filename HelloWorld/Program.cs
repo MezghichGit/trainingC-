743282a7e5f1e651e8a1de2f93c0074af5e19c47
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-20 11:29:36 +0100

    day1 part1

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string formation = "Certification en C#";
+
+            string codeCertif = "20483";
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Formation : {0} le code : {1}", formation, codeCertif);  // message mixte
+            */
+
+            Console.WriteLine("Donner votre nom : ");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Vous êtes : {0}", nom);
+
+        }
+    }
+}
