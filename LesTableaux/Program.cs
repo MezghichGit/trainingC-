ee197282e491814153918fd6cf4b82850164b800
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-20 17:52:23 +0100

    fin exo 2

--- LesTableaux/Program.cs
+++ LesTableaux/Program.cs
@@ -4,9 +4,19 @@ namespace LesTableaux
 {
     class Program
     {
+
+        static void getEtudiantsClasse(int numero, Etudiant[][] tab)
+        {
+            Console.WriteLine("Eleves de la classe : {0}", numero);
+            foreach (Etudiant e in tab[numero])
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
         
         static void Main(string[] args)
         {
+            /*
             Etudiant e1 = new Etudiant(1,"Alain",15.5);
             Etudiant e2 = new Etudiant(2, "Olivier", 17.5);
             Etudiant e3 = new Etudiant(3, "Mathieu", 12.5);
@@ -22,9 +32,6 @@ namespace LesTableaux
                 Console.WriteLine(e.ToString());
             }
 
-
-            // Partie 2
-
             // Affichage la meilleure moyenne
             double MeilleureMoyenne = tab[0].get_moyenne();
             int MeilleurePos = 0;
@@ -39,6 +46,49 @@ namespace LesTableaux
             }
 
             Console.WriteLine("\nL'étudiant avec la meilleure moyenne est {0}", tab[MeilleurePos].ToString());
+
+
+            */
+            Etudiant[][] tabClasses = new Etudiant[3][];
+            tabClasses[0] = new Etudiant[2];
+            tabClasses[1] = new Etudiant[3];
+            tabClasses[2] = new Etudiant[6];
+
+            // remplissage de tabClasses[0]
+            for (int i = 0; i < tabClasses[0].Length; i++)
+            {
+                tabClasses[0][i] = new Etudiant(i, "a-" + i, 12);
+            }
+
+            // remplissage de tabClasses[1]
+            for (int i = 0; i < tabClasses[1].Length; i++)
+            {
+                tabClasses[1][i] = new Etudiant(i, "b-" + i, 14);
+            }
+
+            // remplissage de tabClasses[2]
+            for (int i = 0; i < tabClasses[2].Length; i++)
+            {
+                tabClasses[2][i] = new Etudiant(i, "c-" + i, 13);
+            }
+
+            int k = 1;
+            foreach (Etudiant[] tab in tabClasses)
+            {
+
+                Console.WriteLine("Eleve de la classe : {0} ", k);
+                foreach (Etudiant e in tab)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                k++;
+                    
+            }
+
+            int index = 0;
+
+            getEtudiantsClasse(index, tabClasses);
+            // Partie 2
 
 
 
