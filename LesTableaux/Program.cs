4683ed20e4f3f970fa9326bf1f8b688445fb22c7
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-20 17:02:06 +0100

    exo 2 part 1

--- LesTableaux/Program.cs
+++ LesTableaux/Program.cs
@@ -8,8 +8,23 @@ namespace LesTableaux
         static void Main(string[] args)
         {
             Etudiant e1 = new Etudiant(1,"Alain",15.5);
+            Etudiant e2 = new Etudiant(2, "Olivier", 17.5);
+            Etudiant e3 = new Etudiant(3, "Mathieu", 12.5);
+            Etudiant e4 = new Etudiant(4, "Claude", 11.5);
 
-            Console.WriteLine("Vous êtes : {0}", e1.ToString());
+            Etudiant[] tab = new Etudiant[4];
+            tab[0] = e1;
+            tab[1] = e2;
+            tab[2] = e3;
+            tab[3] = e4;
+            foreach (Etudiant e in tab)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+
+
+            //Console.WriteLine("Vous êtes : {0}", e1.ToString());
             /*
             int compteur = 0;
             int x = 100;
