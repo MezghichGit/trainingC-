77e995e0e34d43c20d303cd0e10e5e736768312d
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-22 12:21:45 +0100

    les indexeurs

---
+++ Day3/Chapitre1_structure_enum/Etudiant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapitre1_structure_enum
+{
+    class Etudiant
+    {
+        public string nom;
+        public int age;
+        public string [] modules;
+
+        public string this[int index]   // indexeur de modules
+        {
+            get { return this.modules[index]; }
+            set { this.modules[index] = value; }
+        }
+
+        public Etudiant(string nom, int age)
+        {
+            this.nom = nom;
+            this.age = age;
+            modules = new string[3];
+        }
+
+    }
+}
---
+++ Day3/Chapitre1_structure_enum/ProgramEtudiant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapitre1_structure_enum
+{
+    class ProgramEtudiant
+    {
+        static void Main(string[] args)
+        {
+            Etudiant e1 = new Etudiant("Amine", 36);
+            e1[0] = "C#";
+            e1[1] = "Test avec C#";
+            e1[2] = "Projet";
+
+            Console.WriteLine("Les modules sont {0} {1} {2}", e1.modules[0], e1[1], e1.modules[2]);
+        }
+    }
+}
