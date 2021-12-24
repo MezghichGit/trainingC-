b3d42cbe971d32a32cdd9436ca2a98d6a85acd71
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-24 14:07:21 +0100

    Serialisation et deserialization en format Json

---
+++ Day5_Module6/Serialization_Deserialization/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_Deserialization
+{
+    class Product
+    {
+
+        public string Name { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public double Price { get; set; }
+        public string[] Sizes { get; set; }
+    }
+}
---
+++ Day5_Module6/Serialization_Deserialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Serialization_Deserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            product.Name = "Apple";
+            product.ExpiryDate = new DateTime(2008, 12, 28);
+            product.Price = 3.99;
+            product.Sizes = new string[] { "Small", "Medium", "Large" };
+
+            string output = JsonConvert.SerializeObject(product);
+            Console.WriteLine(output);
+
+            Product deserializedProduct = JsonConvert.DeserializeObject<Product>(output);
+            Console.WriteLine(deserializedProduct.Name);
+
+
+        }
+    }
+}
