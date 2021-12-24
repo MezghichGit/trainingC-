167af104f5ee1d75ac15703af11cef35d5ac04c0
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-24 16:04:35 +0100

    Exemple 2 avec les StreamWriter et StreamReader

--- Day5_Module6/Chapitre3_LesStrams/Program.cs
+++ Day5_Module6/Chapitre3_LesStrams/Program.cs
@@ -6,6 +6,9 @@ namespace Chapitre3_LesStrams
     {
         static void Main(string[] args)
         {
+
+            // Exemple 1
+            /*
             try
             {
                 // Create an instance of StreamReader to read from a file.
@@ -26,6 +29,31 @@ namespace Chapitre3_LesStrams
                 // Let the user know what went wrong.
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
+            }*/
+
+            //Exemple 2
+
+            // Get the directories currently on the C drive.
+            DirectoryInfo[] cDirs = new DirectoryInfo(@"C:\Users\amine\Desktop\Lab").GetDirectories();
+
+            // Write each directory name to a file.
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\amine\Desktop\Lab\CDriveDirs.txt"))
+            {
+                foreach (DirectoryInfo dir in cDirs)
+                {
+                    sw.WriteLine(dir.Name);
+                }
+            }
+
+            // Read and show each line from the file.
+            string line = "";
+            using (StreamReader sr = new StreamReader(@"C:\Users\amine\Desktop\Lab\CDriveDirs.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
             }
         }
     }
