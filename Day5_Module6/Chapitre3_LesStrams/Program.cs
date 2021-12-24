56f953108c879ad095d1c5f1fa9305de39bd1453
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-24 16:00:15 +0100

    exemple 1 avec les stream - StreamReader

---
+++ Day5_Module6/Chapitre3_LesStrams/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace Chapitre3_LesStrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(@"C:\Users\amine\Desktop\Lab\data.txt"))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
