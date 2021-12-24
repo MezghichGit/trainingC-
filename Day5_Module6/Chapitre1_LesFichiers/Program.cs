932314ea18e90fd42ebab86bfd9b62b7605ce6f7
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-24 12:16:56 +0100

    Exemple avec la classe Path

--- Day5_Module6/Chapitre1_LesFichiers/Program.cs
+++ Day5_Module6/Chapitre1_LesFichiers/Program.cs
@@ -8,6 +8,7 @@ namespace Chapitre1_LesFichiers
     {
         static void Main(string[] args)
         {
+            /*
             string curFile = @"C:\Users\amine\Desktop\Lab\data.txt";
             Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
 
@@ -29,6 +30,53 @@ namespace Chapitre1_LesFichiers
             // Open the file to read from.
             string readText = File.ReadAllText(path);
             Console.WriteLine(readText);
+            */
+
+            // Correction exo 1:
+
+            /*
+            string curFile = @"C:\Users\amine\Desktop\Lab\Exo\eleves.txt";
+            Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
+
+
+            string path = @"C:\Users\amine\Desktop\Lab\Exo\eleves.txt";
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = "Des élèves" + Environment.NewLine;
+                File.WriteAllText(path, createText, Encoding.UTF8); // creation
+            }
+
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            for (int i = 0; i < 3; i++)
+            {
+                string appendText = Console.ReadLine() + Environment.NewLine;
+                File.AppendAllText(path, appendText, Encoding.UTF8); // ajout
+            }
+
+            // Open the file to read from.
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+            */
+
+            // Partie sur les Path
+
+            string fileName1 = "myfile.ext";
+            string fileName2 = @"mydir\myfile";
+            string path = @"C:\mydir.ext\";
+            bool result;
+
+            result = Path.HasExtension(fileName1);
+            Console.WriteLine("HasExtension('{0}') returns {1}", fileName1, result);
+
+            result = Path.HasExtension(fileName2);
+            Console.WriteLine("HasExtension('{0}') returns {1}",fileName2, result);
+
+            result = Path.HasExtension(path);
+            Console.WriteLine("HasExtension('{0}') returns {1}", path, result);
         }
     }
 }
